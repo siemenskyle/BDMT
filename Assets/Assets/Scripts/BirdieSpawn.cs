e09484e7141ac585e24d4a76b32c8eb6fc3d6922
hsj294 <hsj294>
2015-10-18 13:41:51 -0600

    Implemented birdie spawns

---
+++ Assets/Assets/Scripts/BirdieSpawn.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdieSpawn : MonoBehaviour {
+
+	public float x_spawn;
+	public float y_spawn;
+	public bool isPlaying;
+	public KeyCode start;
+	Transform birdTransform;
+
+	// Use this for initialization
+	void Start () {
+		isPlaying = false;
+		birdTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isPlaying) {
+			birdTransform.position = new Vector2(x_spawn, y_spawn);
+		}
+
+		if (Input.GetKey (start)) {
+			isPlaying = true;
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name.Contains("grass"))
+		{
+			isPlaying = false;
+
+			if (birdTransform.position.x < 0) {
+				x_spawn = Mathf.Abs(x_spawn);
+			} else if (birdTransform.position.x > 0) {
+				x_spawn = -1 * Mathf.Abs (x_spawn);
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Assets/Scripts/TimeUpdate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeUpdate : MonoBehaviour {
+    public GameObject timer;
+
+	// Use this for initialization
+	void Start () {
+        //print(Time.realtimeSinceStartup);
+         timer = GameObject.FindGameObjectWithTag("ScoreTimer");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        timer.GetComponent<Text>().text = Time.realtimeSinceStartup.ToString("F1");
+    }
+}
