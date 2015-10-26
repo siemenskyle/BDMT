2969b6d2c3ffb7894e344a42b7a3ebccf287ede2
siemenskyle <siemenskyle@gmail.com>
2015-10-26 15:42:34 -0600

    redesign hits to hitbox style instead of tracing moving racket

--- Assets/Scripts/playermove.cs
+++ Assets/Scripts/playermove.cs
@@ -7,6 +7,8 @@ public class playermove : MonoBehaviour {
 	public float jumpforce;
 	public KeyCode left;
 	public KeyCode right;
+	public KeyCode up;
+	public KeyCode down;
 	public KeyCode jump;
 	public KeyCode hit;
 	Rigidbody2D rbody;
@@ -23,25 +25,39 @@ public class playermove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// Rotate Left
+		// Move Left
 		if (Input.GetKey (left)) {
 			transform.Translate (Vector3.left * speed * Time.deltaTime);
 		}
 		
-		// Rotate Right
+		// Move Right
 		if (Input.GetKey (right)) {
 			transform.Translate (Vector3.right * speed * Time.deltaTime);
+		}
+
+		// Swings
+		if (Input.GetKeyDown (hit)) {
+			// over hit if up held
+			if (Input.GetKey(up))
+				ator.SetBool("over", true);
+			// under hit if down held
+			else if (Input.GetKey(down))
+				ator.SetBool("under", true);
+			// no direction held means normal hit
+			else
+				ator.SetBool("straight", true);
 		}
 	}
 
 	void FixedUpdate () {
-		ator.SetBool ("swing", false);
-		if (Input.GetKey (jump) && transform.position.y < -2.3) {
+		ator.SetBool("over", false);
+		ator.SetBool("straight", false);
+		ator.SetBool("under", false);
+
+		if (Input.GetKeyDown (jump) && transform.position.y < -2.3) {
 			rbody.AddForce (new Vector2 (0, jumpforce));
 		}
 
-		if (Input.GetKey (hit)) {
-			ator.SetBool("swing", true);
-		}
+
 	}
 }
