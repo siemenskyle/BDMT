8d732e6e7151c1a6b905fa308f89acc3c4da0301
U-DESKTOP-VL3250N\Daniel <daniel.blanche@usask.ca>
2015-11-11 16:41:48 -0600

    Helps if I add the script to the commit

---
+++ Assets/Scripts/ShowPower.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowPower : MonoBehaviour {
+
+    public Sprite powerTen;
+    public Sprite powerNine;
+    public Sprite powerEight;
+    public Sprite powerSeven;
+    public Sprite powerSix;
+    public Sprite powerFive;
+    public Sprite powerFour;
+    public Sprite powerThree;
+    public Sprite powerTwo;
+    public Sprite powerOne;
+    public Sprite powerZero;
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch ((int)player.GetComponent<playermove>().specialPower)
+        {
+            case 0:
+                this.GetComponent<SpriteRenderer>().sprite = powerZero;
+                break;
+            case 1:
+                this.GetComponent<SpriteRenderer>().sprite = powerOne;
+                break;
+            case 2:
+                this.GetComponent<SpriteRenderer>().sprite = powerTwo;
+                break;
+            case 3:
+                this.GetComponent<SpriteRenderer>().sprite = powerThree;
+                break;
+             case 4:
+                this.GetComponent<SpriteRenderer>().sprite = powerFour;
+                break;
+            case 5:
+                this.GetComponent<SpriteRenderer>().sprite = powerFive;
+                break;
+            case 6:
+                this.GetComponent<SpriteRenderer>().sprite = powerSix;
+                break;
+            case 7:
+                this.GetComponent<SpriteRenderer>().sprite = powerSeven;
+                break;
+            case 8:
+                this.GetComponent<SpriteRenderer>().sprite = powerEight;
+                break;
+            case 9:
+                this.GetComponent<SpriteRenderer>().sprite = powerNine;
+                break;
+            case 10:
+                this.GetComponent<SpriteRenderer>().sprite = powerTen;
+                break;
+        }
+            
+
+	}
+}
