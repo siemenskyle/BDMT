112001d6fa58039b259e65879826f9b25212bffa
srd807 <srd807@mail.usask.ca>
2015-11-28 17:25:11 -0600

    Quit Button
    added quit button
    fixed scaling on main
    added clickOnQuit script
    added comments

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class LoadOnClick : MonoBehaviour {
 
+	//loads the selected game scene
 	public void LoadScene(int level){
 		
 		Application.LoadLevel (level);
---
+++ Assets/Scripts/OnClickQuit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClickQuit : MonoBehaviour {
+		
+	//quits the entire application
+	public void QuitApplication() {
+
+		Application.Quit ();
+	}
+}
