b6e93d410a95b3941ece80913d797c0ffe11ad7f
siemenskyle <siemenskyle@gmail.com>
2015-12-03 16:59:54 -0600

    better serving indicators

--- Assets/Scripts/ResetIndicators.cs
+++ Assets/Scripts/ResetIndicators.cs
@@ -16,9 +16,10 @@ public class ResetIndicators : StateMachineBehaviour {
 	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		if(GameObject.FindGameObjectWithTag("Bird").GetComponent<BirdieSpawn>().isPlaying)
+			animator.SetTrigger("serve");
+	}
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
