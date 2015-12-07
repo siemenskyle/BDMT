2023ff8e0bd0180e5d176df3b4892ddbe87ee1e6
siemenskyle <siemenskyle@gmail.com>
2015-12-07 15:40:40 -0600

    new music, videos added to rules

---
+++ Assets/Scripts/VideoPlay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VideoPlay : MonoBehaviour {
+
+	MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+		movie = GetComponent<RawImage> ().texture as MovieTexture;
+		movie.Play ();
+		movie.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
